af98bc35bfe9cf2133b1ddaf8283b74b2702262f
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-08-05 00:08:37 -0700

    Added: Ability To Read Or Write The Address Book With Persons Contact As JSON File

--- src/main/java/AddressBookWorkShop/Contact.java
+++ src/main/java/AddressBookWorkShop/Contact.java
@@ -1,5 +1,6 @@
 package AddressBookWorkShop;
 
+
 public class Contact 
 {
 	
@@ -25,68 +26,52 @@ public class Contact
 		this.zipcode = zipcode;
 	}
 	
-	public String getFirstname()
-	{
+	public String getFirstname() {
 		return firstname;
 	}
-	public void setFirstname(String firstname) //first name.
-	{
+	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-	public String getLastname() 
-	{
+	public String getLastname() {
 		return lastname;
 	}
-	public void setLastname(String lastname)//last name.
-	{
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	public String getAddress() 
-	{
+	public String getAddress() {
 		return address;
 	}
-	public void setAddress(String address)//address
-	{
+	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getCity() 
-	{
+	public String getCity() {
 		return city;
 	}
-	public void setCity(String city)//city.
-	{
+	public void setCity(String city) {
 		this.city = city;
 	}
-	public String getState()
-	{
+	public String getState() {
 		return state;
 	}
-	public void setState(String state) //state.
-	{
+	public void setState(String state) {
 		this.state = state;
 	}
-	public String getPhonenumber()
-	{
+	public String getPhonenumber() {
 		return phonenumber;
 	}
-	public void setPhonenumber(String phonenumber)//phone number.
-	{
+	public void setPhonenumber(String phonenumber) {
 		this.phonenumber = phonenumber;
 	}
-	public String getEmailid()
-	{
+	public String getEmailid() {
 		return emailid;
 	}
-	public void setEmailid(String emailid)//emailid.
-	{
+	public void setEmailid(String emailid) {
 		this.emailid = emailid;
 	}
-	public long getZipcode() 
-	{
+	public long getZipcode() {
 		return zipcode;
 	}
-	public void setZipcode(long zipcode)//zipcode.
-	{
+	public void setZipcode(long zipcode) {
 		this.zipcode = zipcode;
 	}
 	
